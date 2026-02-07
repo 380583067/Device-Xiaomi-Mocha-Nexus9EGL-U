8122203399bf710118ab187c303e40617d90f7cd
apple <105837597+380583067@users.noreply.github.com>
2026-02-07 15:17:38 +0800

    Add files via upload

---
+++ lineage_mocha.mk
@@ -0,0 +1,19 @@
+# Inherit device configuration for mocha.
+# Inherit some common lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from xiaomi device
+$(call inherit-product, device/xiaomi/mocha/mocha.mk)
+
+# Product Information
+PRODUCT_NAME := lineage_mocha
+PRODUCT_DEVICE := mocha
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+BOARD_VENDOR := Xiaomi
+PRODUCT_MODEL := MI PAD
+
+# GMS Client ID
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
